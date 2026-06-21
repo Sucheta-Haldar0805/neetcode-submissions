8d503d76ed94c02dff7998202ca33a910c7fadac
Sucheta-Haldar0805 <haldarsucheta5@gmail.com>
2026-06-21 11:43:42 +0530

    Add: median-of-two-sorted-arrays - submission-0

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2= nums2.size();
+        vector<int> temp;
+        int i=0,j=0;
+        while(i<n1 && j<n2){
+            if(nums1[i] <= nums2[j]){
+                temp.push_back(nums1[i++]);
+            }else{
+                temp.push_back(nums2[j++]);
+            }
+        }
+        while(i<n1){
+            temp.push_back(nums1[i++]);
+        }
+        while(j<n2){
+            temp.push_back(nums2[j++]);
+        }
+        int n = n1+n2;
+        if(n%2 != 0){
+            return temp[n/2];
+        }
+        return (double) ((double)(temp[n/2]) + (double)(temp[n/2-1]))/2.0;
+    }
+};
