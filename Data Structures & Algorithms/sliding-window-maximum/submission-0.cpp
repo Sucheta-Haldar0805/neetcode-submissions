a2f4d3ca87a13d0c7d609c8718ab1bb08ed5d318
Sucheta Haldar <haldarsucheta5@gmail.com>
2026-06-28 08:09:51 +0530

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        priority_queue<pair<int,int>> pq;
+
+        for(int i=0;i<k;i++){
+            pq.push(make_pair(nums[i],i));
+        }
+        ans.push_back(pq.top().first);
+
+        for(int i=k;i<nums.size();i++){
+            while(!pq.empty() && pq.top().second <= (i-k)){
+                pq.pop();
+            }
+            pq.push(make_pair(nums[i],i));
+
+            ans.push_back(pq.top().first);
+        }
+        return ans;
+    }
+};
