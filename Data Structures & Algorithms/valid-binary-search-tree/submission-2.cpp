2980165147e3f949313ca40a681579a59191eb65
Sucheta Haldar <haldarsucheta5@gmail.com>
2026-07-07 11:28:48 +0530

    Add: valid-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-2.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool helper(TreeNode* root,TreeNode* maxi,TreeNode* mini){
+        if(root == NULL){
+            return true;
+        }
+        if(mini != NULL && root->val <= mini->val){
+            return false;
+        }
+        if(maxi != NULL && root->val >= maxi->val){
+            return false;
+        }
+
+        return helper(root->left,root,mini) && helper(root->right,maxi,root);
+    }
+    bool isValidBST(TreeNode* root) {
+       return helper(root,NULL,NULL);
+    }
+};
